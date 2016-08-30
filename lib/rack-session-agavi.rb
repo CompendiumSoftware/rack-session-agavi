b31d95a95e8fd4d65de0c4d55148c2eb1ef22ed0
Andrew Morin <andrew.morin@oracle.com>
2016-08-30 09:43:59 -0400

    Fix linter errors and formatting

--- lib/rack-session-agavi.rb
+++ lib/rack-session-agavi.rb
@@ -6,116 +6,115 @@
 require 'rack/session/abstract/id'
 require 'php_serialize'
 require 'dalli'
+
 module Rack
+  module Session
+    #
+    # Rack::Session:Agavi
+    #
+    #
+    class Agavi < Abstract::ID
+      TTL = 100
 
-	module Session
-		#
-		# Rack::Session:Agavi
-		#
-		#
-		class Agavi < Abstract::ID
-	
+      DEFAULT_OPTIONS = Abstract::ID::DEFAULT_OPTIONS.merge \
+        :namespace => '',
+        :key => 'Agavi',
+        :path => '/',
+        :memcache_server => 'localhost:11211'
 
-			TTL = 100
+      def debug(msg)
+        if @logger
+          @logger.debug msg
+        end
+      end
 
-			DEFAULT_OPTIONS = Abstract::ID::DEFAULT_OPTIONS.merge \
-				:namespace => '',
-				:key => 'Agavi',
-				:path => '/',
-				:memcache_server => 'localhost:11211'			
-			def debug(msg)
-				if @logger
-					@logger.debug msg
-				end	
-			end
 
-			
+      def initialize(app, options={})
+        unless options.key?(:key)
+          options[:key] = 'Agavi'
+        end
 
-			def initialize(app, options={})
-				if !options.has_key?(:key)
-					options[:key] = 'Agavi'
-				end
-				options[:path]='/'
-				options[:namespace] = ''
-				options[:namespace_separator] = ''
-				@logger = options[:logger]
+        options[:path] = '/'
+        options[:namespace] = ''
+        options[:namespace_separator] = ''
+        @logger = options[:logger]
 
-				super
-				@default_options = options	
-				@mutex = Mutex.new
-				mserv = @default_options[:memcache_server]
-				mopts = @default_options
+        super
+        @default_options = options
+        @mutex = Mutex.new
+        mserv = @default_options[:memcache_server]
 
-				@pool = options[:cache] || Dalli::Client.new(mserv, {})
-				debug "initialized agavi sessionhandler " + options.inspect
-				debug  'key is '+@key
-			end	
+        @pool = options[:cache] || Dalli::Client.new(mserv, {})
+        debug "initialized agavi sessionhandler #{options.inspect}"
+        debug "key is #{@key}"
+      end
 
 
 
-			def generate_sid
-				loop do
-					sid = super
-					break sid unless @pool.get('session_'+sid)
-				end
-			end
+      def generate_sid
+        loop do
+          sid = super
+          break sid unless @pool.get("session_#{sid}")
+        end
+      end
 
 
-	def	get_session(env, sid)
-				if sid.nil?
-					[sid, nil]
-				end
-				with_lock(env, [nil, {}]) do
-					begin
-					unless sid and session = @pool.get('session_'+sid)
-						debug " sid "+sid +" session "+session.inspect
-						sid, session = generate_sid, PHP.serialize_session({:fromruby => true},assoc=true)
-						unless /^STORED/ =~ @pool.add('session_'+sid, session)
-							raise "Session collision on '#{sid.inspect}'"
-						end
-					end
-					rescue Exception => msg
-						debug "exception caught #{msg.inspect}"
-					end
+      def get_session(env, sid)
+        if sid.nil?
+          [sid, nil]
+        end
+        with_lock(env, [nil, {}]) do
+          begin
+            unless sid && session = @pool.get("session_#{sid}")
+              debug " sid #{sid} session #{session.inspect}"
+              sid = generate_sid
+              session = PHP.serialize_session({:fromruby => true}, true)
 
-					[sid, PHP.unserialize(session)]
-				end
-			end
-			
-			def set_session(env, session_id, new_session, options)
-				expiry = options[:expire_after]
-				expiry = expiry.nil? ? 0 : expiry + 1
+              unless /^STORED/ =~ @pool.add("session_#{sid}", session)
+                raise "Session collision on '#{sid.inspect}'"
+              end
+            end
+          rescue Exception => msg
+            debug "exception caught #{msg.inspect}"
+          end
 
-				# puts "setting SESSION ID "+session_id
-				# with_lock(env, false) do
-				#     @pool.set 'session_'+session_id, PHP.serialize_session(new_session,assoc=true), expiry
-				#     session_id
-				# end
-				session_id
-			end
+          [sid, PHP.unserialize(session)]
+        end
+      end
 
-			def destroy_session(env, session_id, options)
-				with_lock(env) do
-					@pool.delete(session_id)
-					generate_sid unless options[:drop]
-				end
-			end
-			
-			def with_lock(env, default=nil)
-				@mutex.lock if env['rack.multithread']
-				yield
-			rescue Exception
-				if $VERBOSE
-					warn "#{self} is unable to find memcached server."
-					warn $!.inspect
-				end
-				default
-			ensure
-				@mutex.unlock if @mutex.locked?
-			end
-		end
+      def set_session(env, session_id, new_session, options)
+        expiry = options[:expire_after]
+        expiry = expiry.nil? ? 0 : expiry + 1
 
-	end
+        # puts "setting SESSION ID "+session_id
+        # with_lock(env, false) do
+        #     @pool.set 'session_'+session_id, PHP.serialize_session(new_session,assoc=true), expiry
+        #     session_id
+        # end
+        session_id
+      end
+
+      def destroy_session(env, session_id, options)
+        with_lock(env) do
+          @pool.delete(session_id)
+          generate_sid unless options[:drop]
+        end
+      end
+
+      def with_lock(env, default=nil)
+        @mutex.lock if env['rack.multithread']
+        yield
+      rescue Exception
+        if $VERBOSE
+          warn "#{self} is unable to find memcached server."
+          warn $!.inspect
+        end
+        default
+      ensure
+        @mutex.unlock if @mutex.locked?
+      end
+    end
+  end
 end
 
 
--- rack-session-agavi.gemspec
+++ rack-session-agavi.gemspec
@@ -3,16 +3,15 @@
 #
 
 Gem::Specification.new do |s|
+  s.name = 'rack-session-agavi'
+  s.version = '0.1.4'
 
-	s.name = 'rack-session-agavi'
-	s.version = '0.1.4'
-
-	s.authors = ['Stephen Gregory']
-	s.date = '2013-04-24'
-	s.description = "Agavi-compatible session management via rack"
-	s.files = ['rack-session-agavi.gemspec', 'README', 'lib/rack-session-agavi.rb']
-	s.summary = "use agavi sessions in rack based applications"
-	s.add_dependency 'rack', '>= 1.5'
-	s.add_dependency 'php-serialize', '>= 1.2.0'
-    s.add_dependency 'dalli', '1.1.2'	
+  s.authors = ['Stephen Gregory']
+  s.date = '2013-04-24'
+  s.description = "Agavi-compatible session management via rack"
+  s.files = ['rack-session-agavi.gemspec', 'README', 'lib/rack-session-agavi.rb']
+  s.summary = "use agavi sessions in rack based applications"
+  s.add_dependency 'rack', '>= 1.5'
+  s.add_dependency 'php-serialize', '>= 1.2.0'
+  s.add_dependency 'dalli', '1.1.2'
 end
